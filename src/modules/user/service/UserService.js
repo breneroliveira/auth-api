8f71fc6e0c23eca02ecc6f0e56e073bdf5437b85
Brener Augusto de Oliveira <breneroliveirad@gmail.com>
2023-10-12 20:57:32 -0300

    JWT authentication added

--- src/modules/user/service/UserService.js
+++ src/modules/user/service/UserService.js
@@ -1,6 +1,10 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import UserRepository from "../repository/UserRepository.js";
-import * as HttpStatus from "../../../config/constants/HttpStatus.js";
 import UserException from "../exception/UserException.js";
+import * as HttpStatus from "../../../config/constants/HttpStatus.js";
+import * as secrets from "../../../config/constants/Secrets.js";
 
 class UserService {
     async findByEmail(req) {
@@ -34,6 +38,44 @@ class UserService {
     validateUserNotFound(user) {
         if (!user) {
             throw new UserException(HttpStatus.BAD_REQUEST, "User was not found.");
+        }
+    }
+
+    async getAccessToken(req) {
+        try {
+            const { email, password } = req.body;
+            this.validateAccessToken(email, password);
+            let user = await UserRepository.findByEmail(email);
+            this.validateUserNotFound(user);
+            await this.validatePassword(password, user.password);
+            const authUser = { id: user.id, name: user.name, email: user.email };
+            const accessToken = jwt.sign({ authUser }, secrets.API_SECRET, { 
+                expiresIn: "1d" 
+            });
+            return {
+                status: HttpStatus.SUCCESS,
+                accessToken
+            };
+        } catch (err) {
+            return {
+                status: err.status ? err.status : HttpStatus.INTERNAL_SERVER_ERROR,
+                message: err.message
+            };
+        }
+    }
+
+    validateAccessToken(email, password) {
+        if (!email || !password) {
+            throw new UserException(
+                HttpStatus.UNAUTHORIZED, 
+                "Email and password must be informed."
+            );
+        }
+    }
+
+    async validatePassword(password, hashPassword) {
+        if (!await bcrypt.compare(password, hashPassword)) {
+            throw new UserException(HttpStatus.UNAUTHORIZED, "Password doesn't match.");
         }
     }
 }
