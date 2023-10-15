85f4d71759d42212673e9657452313fa920e48e4
Brener Augusto de Oliveira <breneroliveirad@gmail.com>
2023-10-15 20:20:34 -0300

    Authentication middleware added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as db from "./src/config/db/initialData.js";
 import UserRoutes from "./src/modules/user/routes/UserRoutes.js";
+import checkToken from "./src/config/auth/CheckToken.js";
 
 const app = express();
 const env = process.env;
@@ -9,6 +10,7 @@ const PORT = env.PORT || 8080;
 db.createInitialData();
 app.use(express.json());
 app.use(UserRoutes);
+app.use(checkToken);
 
 app.get("/api/status", (req, res) => {
     return res.status(200).json({
