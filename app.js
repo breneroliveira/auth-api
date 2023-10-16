682ac2e8026ef9af1a13a94a5d594b18d292d6ed
Brener Augusto de Oliveira <breneroliveirad@gmail.com>
2023-10-15 22:03:52 -0300

    Endpoint security implemented

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 import express from "express";
 import * as db from "./src/config/db/initialData.js";
 import UserRoutes from "./src/modules/user/routes/UserRoutes.js";
-import checkToken from "./src/config/auth/CheckToken.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
 db.createInitialData();
-app.use(express.json());
-app.use(UserRoutes);
-app.use(checkToken);
 
 app.get("/api/status", (req, res) => {
     return res.status(200).json({
@@ -19,6 +15,9 @@ app.get("/api/status", (req, res) => {
         httpStatus: 200,
     });
 });
+
+app.use(express.json());
+app.use(UserRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started successfully at port ${PORT}.`);
